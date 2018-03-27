35f45c87ef48cf79dbd532072efd4095d224573d
Dave Miller <github@justdave.net>
2018-03-27 16:48:46 -0400

    Base adult nomination unit picker on unit or district recommendation

--- js/adult-nomination.js
+++ js/adult-nomination.js
@@ -6,8 +6,9 @@ $j(document).ready(function(){
   $j('#troopsearch').autocomplete({
     source: nslodge_ajax.ajaxurl + '?action=ns_get_troops_autocomplete',
     select: function( event, ui ) {
-        $j("select[name=ChapterName]").val(ui.item.SelectorName).change();
-        $j("input[name=UnitNumber]").val(ui.item.unit_num);
+        $j("#ChapterName").val(ui.item.SelectorName).change();
+        $j("#UnitNumber").attr('value',ui.item.unit_num);
+        // ^ this doesn't work with .val() for some reason, maybe because type="number"
         $j('#troop_result').html(ui.item.label);
         $j('#troop_picker').hide();
         $j('#troop_picked').show();
@@ -23,10 +24,64 @@ $j(document).ready(function(){
       .appendTo(ul);
     return li;
   }
+  $j('#recommendation').change(function(){
+      if ($j('#recommendation').val() == 'Unit Recommendation') {
+          $j('#unit_unpicked').hide();
+          $j('#district_picker').hide();
+          $j('#district_search').val("");
+          $j('#troop_picker').show();
+          $j('#troop_picked').hide();
+          $j('#district_picked').hide();
+          $j('input[name=UnitType]').val('Troop');
+      }
+      else if ($j('#recommendation').val() == 'District/Council Recommendation') {
+          $j('#unit_unpicked').hide();
+          $j('#district_picker').show();
+          $j('#district_search').val("");
+          $j('#troop_picker').hide();
+          $j('#troop_picked').hide();
+          $j('#district_picked').hide();
+      }
+      else {
+          $j('#unit_unpicked').show();
+          $j('#district_picker').hide();
+          $j('#district_search').val("");
+          $j('#troop_picker').hide();
+          $j('#troop_picked').hide();
+          $j('#district_picked').hide();
+      }
+  });
+  $j('#district_search').change(function(){
+      unittype = 'District';
+      unitnum = $j('#district_search').val();
+      if (unitnum == 'PF') {
+          unittype = 'Council';
+          unitnum = '781';
+          $j('#ChapterName').val('PFFSC Staff').change();
+      }
+      else if (unitnum == 'MCC') {
+          unittype = 'Council';
+          unitnum = '780';
+          $j('#ChapterName').val('MCC Staff').change();
+      }
+      else {
+          $j('#district_picked').show();
+          $j("#ChapterName option[value='']").attr('selected',true);
+      }
+      displaytext = $j('#district_search').find('option:selected').html() + ' - ' + unittype + ' ' + unitnum;
+      $j('#UnitType').val(unittype);
+      $j('#UnitNumber').attr('value',unitnum);
+      // ^ this doesn't work with .val() for some reason, maybe because type="number"
+      $j('#troop_result').html(displaytext);
+      $j('#district_picker').hide();
+      $j('#troop_picker').hide();
+      $j('#troop_picked').show();
+  });
+
+
 });
 
 function change_troop(){
-    $j('#troop_picked').hide();
-    $j('#troop_picker').show();
+    $j('#recommendation').change();
     return false;
 }
