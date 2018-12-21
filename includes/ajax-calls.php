a63f63d07009c68b4a9708805066133aa1c238a6
Dave Miller <github@justdave.net>
2018-12-21 17:54:41 -0500

    refactor js enqueuing code to make it easier to add pages

--- includes/ajax-calls.php
+++ includes/ajax-calls.php
@@ -14,33 +14,31 @@ add_action( 'wp_enqueue_scripts', 'ns_ue_ajax_enqueue_scripts' );
 ns_write_log(__LINE__);
 function ns_ue_ajax_enqueue_scripts() {
     global $wp;
-    if ($_SERVER['REQUEST_URI'] == '/ue/report') {
-        wp_enqueue_script('jquery-ui-autocomplete', '', array('jquery-ui-widget', 'jquery-ui-position'), '1.8.6');
-        wp_enqueue_script( 'ns-report-unit', plugins_url('js/report-unit.js', dirname(__FILE__)), array( 'jquery', 'jquery-form', 'json2' ), false, true );
-        wp_enqueue_style( 'ns-autocomplete-css', plugins_url('css/autocomplete.css', dirname(__FILE__)));
-        # this puts the ajax URL into nslodge_ajax.ajaxurl for the page javascript
-        wp_localize_script( 'ns-report-unit', 'nslodge_ajax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+    $pagelist = array(
+    '/ue/report'            => [ 'pagejs' => 'js/report-unit.js', 'unitwidget' => TRUE ],
+    '/ue/report-scoutentry' => [ 'pagejs' => 'js/report-scout.js' ],
+    '/ue/report-complete'   => [ 'pagejs' => 'js/report-complete.js' ],
+    '/ue/adultnomination'   => [ 'pagejs' => 'js/adult-nomination.js', 'unitwidget' => TRUE ],
+    '/ue/request'           => [ 'pagejs' => 'js/schedule-request.js', 'unitwidget' => TRUE ]
+    );
+    $pageuri = $_SERVER['REQUEST_URI'];
+    if (array_key_exists($pageuri, $pagelist)) {
+        $pagekey = preg_replace('@^/@','',$pageuri);
+        $pagekey = preg_replace('@/@','-',$pagekey);
+        ns_write_log("pagekey = $pagekey");
+        $usesajax = FALSE;
+        if (array_key_exists('unitwidget', $pagelist[$pageuri])) {
+            wp_enqueue_script('jquery-ui-autocomplete', '', array('jquery-ui-widget', 'jquery-ui-position'), '1.8.6');
+            wp_enqueue_style( 'ns-autocomplete-css', plugins_url('css/autocomplete.css', dirname(__FILE__)));
+            $usesajax = TRUE;
+        }
+        wp_enqueue_script( $pagekey, plugins_url($pagelist[$pageuri]['pagejs'], dirname(__FILE__)), array( 'jquery', 'jquery-form', 'json2' ), false, true );
+        if ($usesajax) {
+            # this puts the ajax URL into nslodge_ajax.ajaxurl for the page javascript
+            wp_localize_script( $pagekey, 'nslodge_ajax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+        }
     }
-    if ($_SERVER['REQUEST_URI'] == '/ue/report-scoutentry') {
-        wp_enqueue_script( 'ns-report-scout', plugins_url('js/report-scout.js', dirname(__FILE__)), array( 'jquery', 'jquery-form', 'json2' ), false, true );
-    }
-    if ($_SERVER['REQUEST_URI'] == '/ue/report-complete') {
-        wp_enqueue_script( 'ns-report-complete', plugins_url('js/report-complete.js', dirname(__FILE__)), array( 'jquery', 'jquery-form', 'json2' ), false, true );
-    }
-    if ($_SERVER['REQUEST_URI'] == '/ue/adultnomination') {
-        wp_enqueue_script('jquery-ui-autocomplete', '', array('jquery-ui-widget', 'jquery-ui-position'), '1.8.6');
-        wp_enqueue_script( 'ns-adult-nomination', plugins_url('js/adult-nomination.js', dirname(__FILE__)), array( 'jquery', 'jquery-form', 'json2' ), false, true );
-        wp_enqueue_style( 'ns-autocomplete-css', plugins_url('css/autocomplete.css', dirname(__FILE__)));
-        # this puts the ajax URL into nslodge_ajax.ajaxurl for the page javascript
-        wp_localize_script( 'ns-adult-nomination', 'nslodge_ajax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
-    }
-    if ($_SERVER['REQUEST_URI'] == '/ue/request') {
-        wp_enqueue_script('jquery-ui-autocomplete', '', array('jquery-ui-widget', 'jquery-ui-position'), '1.8.6');
-        wp_enqueue_script( 'ns-schedule-request', plugins_url('js/schedule-request.js', dirname(__FILE__)), array( 'jquery', 'jquery-form', 'json2' ), false, true );
-        wp_enqueue_style( 'ns-autocomplete-css', plugins_url('css/autocomplete.css', dirname(__FILE__)));
-        # this puts the ajax URL into nslodge_ajax.ajaxurl for the page javascript
-        wp_localize_script( 'ns-schedule-request', 'nslodge_ajax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
-    }
+
 }
 # handlers
 add_action( 'wp_ajax_ns_get_troops_autocomplete', 'ns_get_troops_autocomplete' );
