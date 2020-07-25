fd6bdc1f279d0dbb9c17ede7d1dad522506ab3bf
Dave Miller <github@justdave.net>
2020-07-25 16:37:38 -0400

    change map when chapter picked

--- js/chapter-meetings.js
+++ js/chapter-meetings.js
@@ -3,6 +3,13 @@ $j(document).ready(function(){
   $j('.hide').hide();
   $j("#choices").change(function(){
     $j('.hide').slideUp();
-    $j('#'+this.value).slideDown()
+    $j('#'+this.value).slideDown();
+    if (this.value == "none") {
+        $j('#chaptermap').css("background-image", "url(/images/chaptermap2018-thumb.png)");
+        $j("#chaptermaplink").attr("href", "/images/chaptermap2018.png");
+    } else {
+        $j('#chaptermap').css("background-image", "url(/images/chaptermap2018-" + this.value + "-thumb.png)");
+        $j("#chaptermaplink").attr("href", "/images/chaptermap2018-" + this.value + ".png");
+    }
   });
 });
