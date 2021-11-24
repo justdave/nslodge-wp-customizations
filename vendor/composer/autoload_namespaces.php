d9482c83d551c194903417deab5a2604d8365ee2
Dave Miller <github@justdave.net>
2021-11-23 23:27:08 -0500

    Update dependencies:
      - Upgrading markbaker/complex (2.0.0 => 3.0.1)
      - Upgrading markbaker/matrix (2.1.2 => 3.0.0)
      - Upgrading myclabs/php-enum (1.8.0 => 1.8.3)
      - Upgrading phenx/php-font-lib (0.5.1 => 0.5.2)
      - Upgrading phenx/php-svg-lib (v0.3.3 => 0.3.4)
      - Upgrading phpoffice/phpspreadsheet (1.16.0 => 1.20.0)
      - Upgrading sabberworm/php-css-parser (8.3.0 => 8.3.1)
      - Upgrading symfony/polyfill-mbstring (v1.22.1 => v1.23.1)

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Sabberworm\\CSS' => array($vendorDir . '/sabberworm/php-css-parser/lib'),
+    'HTMLPurifier' => array($vendorDir . '/ezyang/htmlpurifier/library'),
 );
